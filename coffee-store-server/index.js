fc5efa86b379cfccbbafb401694e05b356108345
rakibul263 <rakibulhasanshuvo206@gmail.com>
2025-11-16 21:57:54 +0600

    CURD Operation

--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.08bzjfv.mongodb.net/?appName=Cluster0`;
 
 app.get("/", (req, res) => {
   res.send("Coffee Server is running...");
